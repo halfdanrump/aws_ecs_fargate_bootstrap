04963d1d9ff55d828030c01137f0604131b52b9d
Halfdan Rump <dalekk@gmail.com>
2019-08-02 20:43:53 +0900

    cleans up example a bit

--- example.py
+++ example.py
@@ -1,4 +1,3 @@
-import os
 from pprint import pprint
 from projectfiles import (
     ContainerDeployment,
@@ -9,18 +8,7 @@ from projectfiles import (
     ContainerDefinitionsFile,
 )
 
-# name = "zendishes"
-# environment = "production"
-# region = "ap-northeast-1"
-# account_id = "211367837384"
-# awslogs_group = "/aws/ecs/vpc_central/zendishes/production"
-# files = {"buildspec": [BuildspecDockerbuildFile(name=name, environment=environment)]}
-#
-
-# "211367837384.dkr.ecr.ap-northeast-1.amazonaws.com/zendishes_production:latest",
-# image url used in task definition, compose file and buildspec file
-
-# first we setup the project
+# first we setup the project data
 project = Project(
     name="zendishes",
     account_id="211367837384",
@@ -54,12 +42,6 @@ container_definitions = ContainerDefinitionsFile(deployment)
 pprint(container_definitions.document)
 compose_file = DockerComposeFile(image=image)
 
-pprint("*" * 100)
-
-pprint(buildspec.filepath)
-pprint(container_definitions.filepath)
-pprint(compose_file.filepath)
-
 buildspec.write(
     buildspec.dump()
     .replace("'printf", "printf")
@@ -68,29 +50,11 @@ buildspec.write(
 )
 container_definitions.write(container_definitions.dump())
 compose_file.write(compose_file.dump())
-# folders = {
-#     'compose_files': 'compose_files'
-#     ''
-# }
-
 
 # class Bootstrap:
 #     def __init__(self, project: Project):
 #         os.makedirs(f"containers/{project.name}")
 #         os.makedir("buildspec")
-
-
-# folders = {'containers/{zendishes}}
-
-# Create buildspec files
-# buildspec = BuildspecDockerbuildFile(image=zendishes_image)
-
-# Create compose files
-
-# Setup modules folder with config parser
-
-# Setup project folders with
-# - config files
 
 # Setup terraform folder
 # - generate task_definition file
