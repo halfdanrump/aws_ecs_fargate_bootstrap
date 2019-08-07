e8165200c5160c3f455d69cb373fd412bdf9986a
Halfdan Rump <dalekk@gmail.com>
2019-08-07 14:10:56 +0900

    Cleans example

--- example.py
+++ example.py
@@ -56,37 +56,4 @@ task = EcsTask(
 project = Project(config=project_config, tasks=[task])
 
 project.make_files()
-
-# print(project)
-# print(image)
-# print(deployment)
-#
-# # now setup files for CICD
-#
-# # First the buildsec file
-# buildspec = BuildspecDockerbuildFile(image=image)
-# pprint(buildspec.document)
-# container_definitions = ContainerDefinitionsFile(deployment)
-# pprint(container_definitions.document)
-# compose_file = DockerComposeFile(image=image)
-#
-# buildspec.write(
-#     buildspec.dump()
-#     # TODO find a nicer way to do this... Maybe with YAML dump options?
-#     .replace("'printf", "printf")
-#     .replace("''", "'")
-#     .replace("json'", "json")
-# )
-# container_definitions.write(container_definitions.dump())
-# compose_file.write(compose_file.dump())
-#
-# # class Bootstrap:
-# #     def __init__(self, project: Project):
-# #         os.makedirs(f"containers/{project.name}")
-# #         os.makedir("buildspec")
-#
-# # Setup terraform folder
-# # - generate task_definition file
-# # task_definition = TaskDefinitionFile(image=zendishes_image)
-# # - init scheduled_task module
-# # - init cicd module
+project.post_setup()
